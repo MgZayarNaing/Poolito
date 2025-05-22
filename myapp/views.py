9526ef7a5ed0fc165ff53744c9434da1a14ecc21
MgZayarNaing <zayarnaing.st@gmail.com>
2025-05-22 15:54:55 +0630

    heros

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def Home_view(request):
-    return render(request, 'index.html')
+    hero_area = HeroArea.objects.all().order_by('-id')[:1]
+    contex = {
+        'hero_area': hero_area
+    }
+    return render(request, 'index.html',contex)
